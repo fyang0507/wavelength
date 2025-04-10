5504b3477c8e7442972b66d168c99bd39c12f523
fredy <fredy@indeed.com>
2025-04-09 20:16:58 -0400

    set up package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="personal_tldr",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "loguru",
+        "python-dotenv",
+        "requests",
+        "beautifulsoup4",
+        "feedparser",
+    ],
+    python_requires=">=3.11",
+    description="A personal TLDR tool for YouTube channels and podcasts",
+) 
